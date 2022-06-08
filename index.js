a5b42328da28249fb5cc1e3f4688d61ef4589edd
Sylvain Marty <10723351+SylvainMarty@users.noreply.github.com>
2022-06-08 17:37:24 +0200

    Debuging githubEvent imported from GITHUB_EVENT_PATH env

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Action = require('./action')
 
 // eslint-disable-next-line import/no-dynamic-require
 const githubEvent = require(process.env.GITHUB_EVENT_PATH)
+console.log(githubEvent)
 
 async function exec () {
   try {
